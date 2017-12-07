9dd31748f626cf440c98a699d8388aae89fa66d3
Richie Wong <richiewong07@yahoo.com>
2017-12-07 11:47:18 -0600

    updated

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,64 +1,20 @@
 import React, { Component } from 'react';
 
 import Searchbar from '../Searchbar/Searchbar';
-import SearchResults from '../SearchResults/SearchResults';
-
-import axios from 'axios';
 
 class Layout extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      title: ' ',
-      summary: ' ',
-      rating: ' ',
-      image: ' ',
-      genre: [],
-    }
+    this.state = {}
   }
-
-
-  componentDidMount() {
-    axios.get('http://api.tvmaze.com/singlesearch/shows', {
-      params: {
-        q : 'stranger-things'
-      }
-    })
-    .then(response => {
-      this.setState({
-        title: response.data.name,
-        summary: response.data.summary,
-        rating: response.data.rating.average,
-        image: response.data.image.medium,
-        genre: response.data.genres,
-      });
-      console.log(response.data.genres)
-    })
-    .catch(error => {
-      console.log('Error fetching or parsing data'(error))
-    });
-  }
-
 
   render () {
-
-    let editedSummary = this.state.summary.replace(/(<p>|<b>|<\/p>|<\/b>)/g, '')
-
-    let spiltGenre = this.state.genre.join(', ');
-
     return(
       <div>
         <div>Toolbar</div>
         <div>Sidedrawer</div>
         <Searchbar/>
-        <SearchResults
-        showTitle={this.state.title}
-        showSummary={editedSummary}
-        showRating={this.state.rating}
-        showImage={this.state.image}
-        showGenre={spiltGenre}
-      />
-    </div>
+      </div>
     )
   }
 }
--- src/Searchbar/Searchbar.jsx
+++ src/Searchbar/Searchbar.jsx
@@ -1,37 +1,74 @@
 import React, { Component } from 'react';
 
+import SearchResults from '../SearchResults/SearchResults';
+import axios from 'axios';
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ' '
+      searchTerm: ' ',
+      title: ' ',
+      summary: ' ',
+      rating: ' ',
+      image: ' ',
+      genre: [],
     }
   }
 
-
-  showSearchHandler = (event) => {
-    this.setState({searchTerm: event.target.value})
-    console.log(event.target.value)
+  componentWillMount() {
+    this.search();
   }
 
-  submitHandler = (event) => {
-    console.log('Submitted: ', this.state.searchTerm);
-    event.preventDefault();
+
+  updateSearchHandler = (event) => {
+    this.search(this.refs.query.value);
+  }
+
+  // submitHandler = (event) => {
+  //   console.log('Submitted: ', this.state.searchTerm);
+  //   event.preventDefault();
+  // }
+
+  search(query = "stranger-things") {
+    const url = `http://api.tvmaze.com/singlesearch/shows?q=${query}`;
+    axios.get(url).then(response => {
+      this.setState({
+        title: response.data.name,
+        summary: response.data.summary,
+        rating: response.data.rating.average,
+        image: response.data.image.medium,
+        genre: response.data.genres,
+      });
+      console.log(response.data.genres)
+    })
+    // .catch(error => {
+    //   console.log('Error fetching or parsing data'(error))
+    // })
   }
 
 
   render() {
+    let editedSummary = this.state.summary.replace(/(<p>|<b>|<\/p>|<\/b>)/g, '')
+
+    let spiltGenre = this.state.genre.join(', ');
+
     return(
       <div>
         <form onSubmit={this.submitHandler}>
         <input
           type="text"
           ref="query"
-          value={this.state.searchTerm}
-          onChange={this.showSearchHandler} />
+          onChange={this.updateSearchHandler} />
         <button type="submit">Search</button>
         </form>
+        <SearchResults
+        showTitle={this.state.title}
+        showSummary={editedSummary}
+        showRating={this.state.rating}
+        showImage={this.state.image}
+        showGenre={spiltGenre}
+      />
       </div>
     )
   }
